d45afae19f93846ac5fde402f622cb319909a7a4
Andrey_Ionel <a-ionel@ukr.net>
2023-06-20 17:59:52 +0300

    Add project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      {
+        additionalHooks: '^use.+(Callback)$',
+      },
+    ],
+  },
 };
+
